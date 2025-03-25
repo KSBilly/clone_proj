3775233783ab1f5455b647953ec59cdfeee7cb9d
Leonardo <leonardo@aulas.com>
2025-03-25 10:39:34 -0300

    Criando o Hero e importando fontes

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function style(){
     return gulp.src("./src/styles/*.scss")
@@ -7,7 +8,14 @@ function style(){
     .pipe(gulp.dest('./build/styles'))
 }
 
-exports.default = style;
+function images(){
+    return gulp.src("./src/images/**/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest('./build/images'));
+}
+
+exports.default = gulp.parallel(style);
+
 exports.watch = function(){
     gulp.watch('./scr/styles/*.scss', gulp.parallel(style));
 }
